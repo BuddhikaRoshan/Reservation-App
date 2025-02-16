ead6f25132c997b055e20b6b560ceffa8a40d03a
BuddhikaRoshan <buddhikaroshanofficial@gmail.com>
2025-02-16 21:36:44 +0530

    add files

---
+++ app/src/main/java/com/example/myapplicationb/MainActivity11.kt
@@ -0,0 +1,19 @@
+package com.example.myapplicationb
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity11 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shedule)
+
+        val constraintLayout: ConstraintLayout = findViewById(R.id.mainLayout)
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setExitFadeDuration(5000)
+        animationDrawable.start()
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationb/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.myapplicationb
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.intro)
+
+        val constraintLayout: ConstraintLayout = findViewById(R.id.mainLayout)
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setExitFadeDuration(5000)
+        animationDrawable.start()
+
+
+
+
+        }
+    }
